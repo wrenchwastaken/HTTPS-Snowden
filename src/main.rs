2d5257602ba7d33da60bf73527c55fc16524c215
Soyam Das <soyammun@gmail.com>
2023-10-18 22:27:25 +0530

    pass 4th stage

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,33 @@
 use std::io::{BufRead, BufReader, Write};
 use std::net::TcpListener;
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
+fn main(){
     println!("Logs from your program will appear here!");
-    // Uncomment this block to pass the first stage
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-    for stream in listener.incoming() {
+    for stream in listener.incoming(){
         match stream {
-            Ok(mut _stream) => {
-                let reader = BufReader::new(&_stream);
+            Ok(mut stream) => {
+                let reader = BufReader::new(&stream);
                 let mut lines = reader.lines();
-                let first_line = lines.next().unwrap();
-                match first_line {
-                    Ok(text) => {
-                        let mut datas = text.split_whitespace();
-                        let _method = datas.next().unwrap();
-                        let path = datas.next().unwrap();
-                        if path == "/" {
-                            let _ = _stream.write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes());
-                        } else {
-                            let _ = _stream.write_all("HTTP/1.1 404 NOT FOUND\r\n\r\n".as_bytes());
-                        }
+                let request = lines.next().unwrap().unwrap();
+                let path = request.split_whitespace().nth(1).unwrap();
+                match path{
+                    "/" => {
+                        stream.write(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
                     }
-                    Err(_) => {
-                        println!("no line found")
+                    _ => {
+                        println!("request path {:?}",path);
+                        if path.starts_with("/echo/") {
+                            let content = path.split("/echo/").nth(1).unwrap();
+                            let response = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}\n",content.len(),content);
+                            stream.write(response.as_bytes()).unwrap();
+                        } else {
+                            stream.write(b"HTTP/1.1 404 Not Found\r\n\r\n").unwrap();
+                        }
                     }
                 }
             }
             Err(e) => {
-                println!("error: {}", e);
+                println!("error: {}",e);
             }
         }
     }

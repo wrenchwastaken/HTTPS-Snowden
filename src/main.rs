4fb0d461e1b9fc5192f77adcb6ad91aed605e629
Soyam Das <soyammun@gmail.com>
2023-10-18 21:29:59 +0530

    pass 3rd stage

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,35 @@
-// Uncomment this block to pass the first stage
-use std::io::Read;
-use std::io::Write;
+use std::io::{BufRead, BufReader, Write};
 use std::net::TcpListener;
-use std::net::TcpStream;
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
     // Uncomment this block to pass the first stage
-    //
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-    //
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
-                println!("New Connection Accepted");
-
-                handle_client(_stream);
+            Ok(mut _stream) => {
+                let reader = BufReader::new(&_stream);
+                let mut lines = reader.lines();
+                let first_line = lines.next().unwrap();
+                match first_line {
+                    Ok(text) => {
+                        let mut datas = text.split_whitespace();
+                        let _method = datas.next().unwrap();
+                        let path = datas.next().unwrap();
+                        if path == "/" {
+                            let _ = _stream.write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes());
+                        } else {
+                            let _ = _stream.write_all("HTTP/1.1 404 NOT FOUND\r\n\r\n".as_bytes());
+                        }
+                    }
+                    Err(_) => {
+                        println!("no line found")
+                    }
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
-}
-fn handle_client(mut stream: TcpStream) {
-    let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
 }
